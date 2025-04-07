35abe6fd15d0a395994002a06a7d0bcc8f5d3d49
nicholasloureiro <nicholoureiro@gmail.com>
2025-04-07 12:22:11 -0300

    add response feature

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from evolution_api import send_whatsapp_message
 
 app = FastAPI()
 
@@ -6,5 +7,13 @@ app = FastAPI()
 async def webhook(request: Request):
     data = await request.json()
     print(data)
+    chat_id = data.get('data').get('key').get('remoteJid')
+    message = data.get('data').get('message').get('conversation')
+
+    if chat_id and message and not '@g.us' in chat_id:
+        send_whatsapp_message(
+            number=chat_id,
+            text='.'
+        )
     return {'status': 'ok'}
 
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+EVOLUTION_API_URL=os.getenv('EVOLUTION_API_URL')
+
+EVOLUTION_INSTANCE_NAME=os.getenv('EVOLUTION_INSTANCE_NAME')
+
+AUTHENTICATION_API_KEY=os.getenv('AUTHENTICATION_API_KEY')
