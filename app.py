835cce1b55b63568fbd2a8ecc3474694c62f8c20
nicholasloureiro <nicholoureiro@gmail.com>
2025-04-07 16:34:14 -0300

    implement rag

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Request
 from evolution_api import send_whatsapp_message
+from chains import get_conversational_rag_chain
 
 app = FastAPI()
+
+convertional_rag_chain =  get_conversational_rag_chain()
 
 @app.post('/webhook')
 async def webhook(request: Request):
@@ -11,9 +14,13 @@ async def webhook(request: Request):
     message = data.get('data').get('message').get('conversation')
 
     if (chat_id and message and not '@g.us' in chat_id) and ('553493090525' in chat_id or '553491143442' in chat_id or '553492999993' in chat_id):
+        ai_response = convertional_rag_chain.invoke(
+            input={'input': message},
+            config={'configurable':{'session_id':chat_id}},
+        ).get('answer')
         send_whatsapp_message(
             number=chat_id,
-            text=f'{message}'
+            text=ai_response,
         )
     return {'status': 'ok'}
 
---
+++ chains.py
@@ -0,0 +1,29 @@
+from langchain.chains import create_history_aware_history_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI
+from config import OPENAI_MODEL_NAME, OPENAI_MODEL_TEMPERATURE
+from vectorstore import get_vector_store
+from prompts import contextualize_prompt, qa_prompt
+from memory import get_session_history
+
+def get_rag_chain():
+    llm = ChatOpenAI(OPENAI_MODEL_NAME, OPENAI_MODEL_TEMPERATURE)
+    retriever = get_vector_store().as_retriever()
+    history_aware_chain = create_history_aware_history_retriever(llm, retriever, contextualize_prompt)
+    question_answer_chain = create_stuff_documents_chain(
+        llm=llm,
+        prompt=qa_prompt,
+    )
+    return create_retrieval_chain(history_aware_chain, question_answer_chain)
+
+
+def get_conversational_rag_chain():
+    rag_chain = get_rag_chain()
+    return RunnableWithMessageHistory(
+        runnable=rag_chain,
+        get_session_history=get_session_history,
+        input_messages_key='input',
+        history_messages_key='chat_history',
+        output_messages_key='answer'
+    )
