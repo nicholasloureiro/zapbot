e4854a40966418daa6a8328273dfd1a6ea726395
nicholasloureiro <nicholoureiro@gmail.com>
2025-04-07 17:10:01 -0300

    add file

--- vectorstore.py
+++ vectorstore.py
@@ -1,18 +1,21 @@
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
 import os
 import shutil
-from config import RAG_FILES_DIR, VECTOR_STORE_PATH
+
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 
+from config import RAG_FILES_DIR, VECTOR_STORE_PATH
+
+
 def load_documents():
     docs = []
-    processed_dir = os.path.join(RAG_FILES_DIR,'processed')
+    processed_dir = os.path.join(RAG_FILES_DIR, 'processed')
     os.makedirs(processed_dir, exist_ok=True)
 
     files = [
-        os.path.join(RAG_FILES_DIR,f)
+        os.path.join(RAG_FILES_DIR, f)
         for f in os.listdir(RAG_FILES_DIR)
         if f.endswith('.pdf') or f.endswith('.txt')
     ]
@@ -22,24 +25,23 @@ def load_documents():
         docs.extend(loader.load())
         dest_path = os.path.join(processed_dir, os.path.basename(file))
         shutil.move(file, dest_path)
-    
+
     return docs
 
-def get_vector_store():
+def get_vectorstore():
     docs = load_documents()
     if docs:
         text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap=200,
+            chunk_size=1000,
+            chunk_overlap=200,
         )
         splits = text_splitter.split_documents(docs)
         return Chroma.from_documents(
             documents=splits,
             embedding=OpenAIEmbeddings(),
-            persist_directory=VECTOR_STORE_PATH
+            persist_directory=VECTOR_STORE_PATH,
         )
-    
     return Chroma(
-        embedding_function=OpenAIEmbeddings,
-        persist_directory=VECTOR_STORE_PATH
+        embedding_function=OpenAIEmbeddings(),
+        persist_directory=VECTOR_STORE_PATH,
     )
