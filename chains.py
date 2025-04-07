9d9ef4efb093d0df562993a49650063854248a35
nicholasloureiro <nicholoureiro@gmail.com>
2025-04-07 17:03:43 -0300

    fix import error

--- chains.py
+++ chains.py
@@ -1,4 +1,4 @@
-from langchain.chains import create_history_aware_history_retriever, create_retrieval_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI
@@ -10,7 +10,7 @@ from memory import get_session_history
 def get_rag_chain():
     llm = ChatOpenAI(OPENAI_MODEL_NAME, OPENAI_MODEL_TEMPERATURE)
     retriever = get_vector_store().as_retriever()
-    history_aware_chain = create_history_aware_history_retriever(llm, retriever, contextualize_prompt)
+    history_aware_chain = create_history_aware_retriever(llm, retriever, contextualize_prompt)
     question_answer_chain = create_stuff_documents_chain(
         llm=llm,
         prompt=qa_prompt,
